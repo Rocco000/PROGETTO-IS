ebdc771846634526762444e9f2296620b89f1ad0
39338 <39338@LAPTOP-7RN40MVN.home-life.hub>
2021-05-08 11:18:52 +0200

    Ho creato i bean e i modelDAO delle tabelle Console,CorriereEspresso,Disponibile,Dlc,Fornitore -> AntonioM.

---
+++ RAAF-GAMING/src/it/unisa/model/CorriereEspressoModelDAO.java
@@ -0,0 +1,38 @@
+package it.unisa.model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CorriereEspressoModelDAO implements OperazioniModel<CorriereEspressoBean> {
+
+	@Override
+	public CorriereEspressoBean doRetriveByKey(String code) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<CorriereEspressoBean> doRetriveAll(String order) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void doSave(CorriereEspressoBean item) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doUpdate(CorriereEspressoBean item) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doDelete(CorriereEspressoBean item) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ RAAF-GAMING/src/it/unisa/model/FornitoreModelDAO.java
@@ -0,0 +1,38 @@
+package it.unisa.model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class FornitoreModelDAO implements OperazioniModel<FornitoreBean> {
+
+	@Override
+	public FornitoreBean doRetriveByKey(String code) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<FornitoreBean> doRetriveAll(String order) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void doSave(FornitoreBean item) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doUpdate(FornitoreBean item) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doDelete(FornitoreBean item) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
