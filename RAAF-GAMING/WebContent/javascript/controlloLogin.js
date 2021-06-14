8ab9a59185f7d02d820592dc7f18d71eac27bcde
Rocco000 <r.iuliano13@studenti.unisa.it>
2021-06-14 17:30:39 +0200

    login.jsp con css e javasript e immagini -> rocco

---
+++ RAAF-GAMING/WebContent/javascript/controlloLogin.js
@@ -0,0 +1,31 @@
+
+
+function controlloValori(x){
+	var e= x.elements["email"];
+	
+	var mailformat=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if(e.value.match(mailformat)){//controllo formato email
+		
+		var p= x.elements["password"]; //controllo lunghezza password
+		var valore= p.value.length;
+		if(valore<10){
+			alert("You have entered an invalid password!");
+			p.value="";
+			p.style.border="2px solid red";
+			return false;
+		}
+		else
+			return true;
+		
+	}else{
+		alert("You have entered an invalid email address!");
+		e.value="";
+		e.style.border="2px solid red";
+		return false;
+	}
+	
+	
+}
+
+
+
