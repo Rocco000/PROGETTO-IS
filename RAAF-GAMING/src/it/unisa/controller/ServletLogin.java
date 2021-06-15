847588c74a8a932f35fa9f01f278163c86ac42c0
Rocco000 <r.iuliano13@studenti.unisa.it>
2021-06-15 14:11:33 +0200

    servlet di login e logout -> rocco

--- RAAF-GAMING/src/it/unisa/controller/ServletLogin.java
+++ RAAF-GAMING/src/it/unisa/controller/ServletLogin.java
@@ -2,7 +2,6 @@ package it.unisa.controller;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
@@ -13,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
 import it.unisa.model.ClienteBean;
@@ -36,21 +36,44 @@ public class ServletLogin extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String email=request.getParameter("email");
-		String password=request.getParameter("password");
-		ClienteModelDAO cmd=new ClienteModelDAO((DataSource)super.getServletContext().getAttribute("DataSource"));
-		try {
-			ClienteBean cb=cmd.doRetriveByKey(email);
-			if(cb!=null) {
-				MessageDigest md = MessageDigest.getInstance("MD5"); 
-				byte[] digest = md.digest(password.getBytes()); 
-				BigInteger big=new BigInteger(1,digest);
-				String str=big.toString(16);
-				while(str.length()<32) {
-					str="0"+str;
-				}
-				if(str.equals(cb.getPassword())) {
-					System.out.println("Sei loggato");
+		
+		HttpSession sessioneExists= request.getSession(false);//controllo se esiste già una sessione
+		if(sessioneExists!=null) {
+			RequestDispatcher dispatcher=super.getServletContext().getRequestDispatcher("index.jsp");//ritorno alla index
+			dispatcher.forward(request, response);
+		}		
+		else {
+			String email=request.getParameter("email");
+			String password=request.getParameter("password");
+			ClienteModelDAO cmd=new ClienteModelDAO((DataSource)super.getServletContext().getAttribute("DataSource"));
+			try {
+				ClienteBean cb=cmd.doRetriveByKey(email);
+				if(cb!=null) {
+					MessageDigest md = MessageDigest.getInstance("MD5"); 
+					byte[] digest = md.digest(password.getBytes()); 
+					BigInteger big=new BigInteger(1,digest);
+					String str=big.toString(16);
+					while(str.length()<32) {
+						str="0"+str;
+					}
+					if(str.equals(cb.getPassword())) {   //se la password e l'email coincidono nel DB
+						System.out.println("Sei loggato");
+						HttpSession sessione= request.getSession(true);//crea la sessione
+						sessione.setAttribute("emailSession", email);
+						sessione.setAttribute("passwordSession", str);
+						boolean flag= true;
+						request.setAttribute("omino", flag);
+						String url="index.jsp";
+						url=response.encodeURL(url);
+						RequestDispatcher dispatcher=super.getServletContext().getRequestDispatcher(url);
+						dispatcher.forward(request, response);
+					}
+					else {
+						String message="";
+						request.setAttribute("message",message);
+						RequestDispatcher view=super.getServletContext().getRequestDispatcher("/login.jsp");
+						view.forward(request, response);
+					}
 				}
 				else {
 					String message="";
@@ -58,14 +81,13 @@ public class ServletLogin extends HttpServlet {
 					RequestDispatcher view=super.getServletContext().getRequestDispatcher("/login.jsp");
 					view.forward(request, response);
 				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-		
 	}
 
 }
---
+++ RAAF-GAMING/src/it/unisa/controller/ServletLogout.java
@@ -0,0 +1,44 @@
+package it.unisa.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 
+ * Servlet per invalidare la sessione
+ *
+ */
+
+@WebServlet("/servletlogout")
+public class ServletLogout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServletLogout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	HttpSession sessione= request.getSession(false);//ottengo la sessione
+    	sessione.removeAttribute("emailSession");
+    	sessione.removeAttribute("passwordSession");
+    	sessione.invalidate();//elimino la sessione
+    	boolean flag=(boolean) request.getAttribute("omino");
+    	flag=false;
+    	request.setAttribute("omino", flag);
+    	RequestDispatcher dispatcher=super.getServletContext().getRequestDispatcher("index.jsp");//ritorno alla index
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
