589747e070b14747df2397bb1ccefa0fe8def909
39338 <39338@LAPTOP-7RN40MVN>
2021-06-30 11:11:26 +0200

    servletprodottonuovoadmin terminata

--- RAAF-GAMING/src/it/unisa/controller/ServletFormProdNuovoAdmin.java
+++ RAAF-GAMING/src/it/unisa/controller/ServletFormProdNuovoAdmin.java
@@ -24,6 +24,10 @@ import it.unisa.model.ConsoleBean;
 import it.unisa.model.ConsoleModelDAO;
 import it.unisa.model.DlcBean;
 import it.unisa.model.DlcModelDAO;
+import it.unisa.model.MagazzinoBean;
+import it.unisa.model.MagazzinoModelDAO;
+import it.unisa.model.PresenteInBean;
+import it.unisa.model.PresenteInModelDAO;
 import it.unisa.model.ProdottoBean;
 import it.unisa.model.ProdottoModelDAO;
 import it.unisa.model.VideogiocoBean;
@@ -84,124 +88,163 @@ public class ServletFormProdNuovoAdmin extends HttpServlet {
 						}
 						else //il nuovo prodotto non e presente nel database
 						{
-							System.out.println("dopo else");
-							ProdottoBean prodBean = new ProdottoBean();
-							ProdottoBean max =  ProdDAO.getMax();
-							prodBean.setCodice_prodotto(max.getCodice_prodotto()+1);
-							prodBean.setData_uscita(dataProd);
-							prodBean.setFornitore(fornitoreProd);
-							//prendere il file dalla request
-							System.out.println("prima di request.getpart");
-							Part parte = request.getPart("copertina");
-							System.out.println("dopo request.getpart");
-							InputStream flusso = null;
-							
-							System.out.println("prima di if");
-							if(parte!=null)
-							{
-								flusso=parte.getInputStream();
-								prodBean.setCopertina(flusso.readAllBytes());
-							}
-							System.out.println("dopo if");
-							
-							prodBean.setNome(nomeProd);
-							prodBean.setPrezzo(prezzoProd);
-							prodBean.setQuantita_fornitura(quantitaProd);
-							prodBean.setSconto(scontoProd);
-							Date dateatt = new Date(Calendar.getInstance().getTime().getTime());
-							prodBean.setUltima_fornitura(dateatt);
-							
-							String scelta = request.getParameter("sceltaP");
-							
-							if(scelta.equals("videogioco")==true)
-							{
-								double dimensione  =Double.parseDouble(request.getParameter("dimensioni"));
-								int pegi = Integer.parseInt(request.getParameter("pegi"));
-								int ncd = Integer.parseInt(request.getParameter("ncd"));
-								String chiave = request.getParameter("chiave");
-								String nomesfh = request.getParameter("nomesfh");
-								boolean edlim = Boolean.parseBoolean(request.getParameter("limitata"));
-								
-								VideogiocoBean videogicocoBean = new VideogiocoBean();
-								videogicocoBean.setDimensione(dimensione);
-								videogicocoBean.setEdizione_limitata(edlim);
-								videogicocoBean.setPegi(pegi);
-								videogicocoBean.setSoftware_house(nomesfh);
-								videogicocoBean.setProdotto(prodBean.getCodice_prodotto());
-								
-						
-								
-								boolean flag=false;
-								
-								if(ncd>0)
+							//vedo se nel magazino c'e spazio
+							MagazzinoModelDAO mmd=new MagazzinoModelDAO(ds);
+							ArrayList<MagazzinoBean> mb=null;
+							try {
+								mb=mmd.doRetriveAll("");
+								} 
+								catch (SQLException e)
 								{
-									flag=true;
+								e.printStackTrace();
 								}
-								if(flag==true)
+							PresenteInModelDAO pid=new PresenteInModelDAO(ds);
+							for(int i=0;i<mb.size();i++)
 								{
-									//inserisci un gioco fisico
-									videogicocoBean.setNcd(ncd);
-									videogicocoBean.setVkey(null);
-								}
-								else
-								{
-									//inserisci gioco digitale
-									videogicocoBean.setVkey(chiave);
-									videogicocoBean.setNcd(0);
-								}
-								
-								ProdDAO.doSave(prodBean);
-								VideogiocoModelDAO vDAO = new VideogiocoModelDAO(ds);
-								vDAO.doSave(videogicocoBean);
-								
-							}
-							else if(scelta.equals("console")==true)
-							{
-								String specifica = request.getParameter("specifiche");
-								String colore = request.getParameter("colore");
-								
-								ConsoleBean console = new ConsoleBean();
-								console.setColore(colore);
-								console.setProdotto(prodBean.getCodice_prodotto());
-								console.setSpecifica(specifica);
-								
-								ConsoleModelDAO cDAO = new ConsoleModelDAO(ds);
-								ProdDAO.doSave(prodBean);
-								cDAO.doSave(console);
-							}
-							else if(scelta.equals("dlc")==true)
-							{
-								String descrizione = request.getParameter("descrizione");
-								int dimensioneDlc = Integer.parseInt(request.getParameter("dimensioneDlc"));
-								
-								DlcBean dlcbean = new DlcBean();
-								DlcModelDAO dDAO = new DlcModelDAO(ds);
-								dlcbean.setProdotto(prodBean.getCodice_prodotto());
-								dlcbean.setDescrizione(descrizione);
-								dlcbean.setDimensione(dimensioneDlc);
+									ArrayList<PresenteInBean> pb=null;
+									try 
+									{
+										pb=pid.doRetriveByMagazzino(mb.get(i).getIndirizzo());
+									}
+										catch (SQLException e)
+										{
+										e.printStackTrace();
+										}
+										int cont=0;
+										for(PresenteInBean pib:pb)
+										{
+											cont+=pib.getQuantita_disponibile();
+										}
+									if((cont+quantitaProd)<=mb.get(i).getCapienza()) 
+										{//disponibilita in magazzino
+										
+										ProdottoBean prodBean = new ProdottoBean();
+										ProdottoBean max =  ProdDAO.getMax();
+										prodBean.setCodice_prodotto(max.getCodice_prodotto()+1);
+										prodBean.setData_uscita(dataProd);
+										prodBean.setFornitore(fornitoreProd);
+										//prendere il file dalla request
+										System.out.println("prima di request.getpart");
+										Part parte = request.getPart("copertina");
+										System.out.println("dopo request.getpart");
+										InputStream flusso = null;
+										
+										System.out.println("prima di if");
+										if(parte!=null)
+										{
+											flusso=parte.getInputStream();
+											prodBean.setCopertina(flusso.readAllBytes());
+										}
+										System.out.println("dopo if");
+										
+										prodBean.setNome(nomeProd);
+										prodBean.setPrezzo(prezzoProd);
+										prodBean.setQuantita_fornitura(quantitaProd);
+										prodBean.setSconto(scontoProd);
+										Date dateatt = new Date(Calendar.getInstance().getTime().getTime());
+										prodBean.setUltima_fornitura(dateatt);
+										
+										String scelta = request.getParameter("sceltaP");
+										
+										if(scelta.equals("videogioco")==true)
+										{
+											double dimensione  =Double.parseDouble(request.getParameter("dimensioni"));
+											int pegi = Integer.parseInt(request.getParameter("pegi"));
+											int ncd = Integer.parseInt(request.getParameter("ncd"));
+											String chiave = request.getParameter("chiave");
+											String nomesfh = request.getParameter("nomesfh");
+											boolean edlim = Boolean.parseBoolean(request.getParameter("limitata"));
+											
+											VideogiocoBean videogicocoBean = new VideogiocoBean();
+											videogicocoBean.setDimensione(dimensione);
+											videogicocoBean.setEdizione_limitata(edlim);
+											videogicocoBean.setPegi(pegi);
+											videogicocoBean.setSoftware_house(nomesfh);
+											videogicocoBean.setProdotto(prodBean.getCodice_prodotto());
+											
+									
+											
+											boolean flag=false;
+											
+											if(ncd>0)
+											{
+												flag=true;
+											}
+											if(flag==true)
+											{
+												//inserisci un gioco fisico
+												videogicocoBean.setNcd(ncd);
+												videogicocoBean.setVkey(null);
+											}
+											else
+											{
+												//inserisci gioco digitale
+												videogicocoBean.setVkey(chiave);
+												videogicocoBean.setNcd(0);
+											}
+											
+											ProdDAO.doSave(prodBean);
+											VideogiocoModelDAO vDAO = new VideogiocoModelDAO(ds);
+											vDAO.doSave(videogicocoBean);
+											
+										}
+										else if(scelta.equals("console")==true)
+										{
+											String specifica = request.getParameter("specifiche");
+											String colore = request.getParameter("colore");
+											
+											ConsoleBean console = new ConsoleBean();
+											console.setColore(colore);
+											console.setProdotto(prodBean.getCodice_prodotto());
+											console.setSpecifica(specifica);
+											
+											ConsoleModelDAO cDAO = new ConsoleModelDAO(ds);
+											ProdDAO.doSave(prodBean);
+											cDAO.doSave(console);
+										}
+										else if(scelta.equals("dlc")==true)
+										{
+											String descrizione = request.getParameter("descrizione");
+											int dimensioneDlc = Integer.parseInt(request.getParameter("dimensioneDlc"));
+											
+											DlcBean dlcbean = new DlcBean();
+											DlcModelDAO dDAO = new DlcModelDAO(ds);
+											dlcbean.setProdotto(prodBean.getCodice_prodotto());
+											dlcbean.setDescrizione(descrizione);
+											dlcbean.setDimensione(dimensioneDlc);
+										
+											ProdDAO.doSave(prodBean);
+											dDAO.doSave(dlcbean);
+										}
+										else
+										{
+											String codice = request.getParameter("codice");
+											int durata = Integer.parseInt(request.getParameter("durata"));
+											
+											AbbonamentoBean abb = new AbbonamentoBean();
+											AbbonamentoModelDAO abbDAO = new AbbonamentoModelDAO(ds);
+											
+											abb.setCodice(codice);
+											abb.setDurata_abbonamento(durata);
+											abb.setProdotto(prodBean.getCodice_prodotto());
+											
+											ProdDAO.doSave(prodBean);
+											abbDAO.doSave(abb);
+										}
+										String messageok = "Prodotto inserito con successo!";
+										request.setAttribute("messageok", messageok);
+										RequestDispatcher dispatcher = request.getRequestDispatcher(response.encodeURL("/servletgestioneadmin"));
+										dispatcher.forward(request, response);
+										}
+								}	
 							
-								ProdDAO.doSave(prodBean);
-								dDAO.doSave(dlcbean);
-							}
-							else
-							{
-								String codice = request.getParameter("codice");
-								int durata = Integer.parseInt(request.getParameter("durata"));
-								
-								AbbonamentoBean abb = new AbbonamentoBean();
-								AbbonamentoModelDAO abbDAO = new AbbonamentoModelDAO(ds);
-								
-								abb.setCodice(codice);
-								abb.setDurata_abbonamento(durata);
-								abb.setProdotto(prodBean.getCodice_prodotto());
-								
-								ProdDAO.doSave(prodBean);
-								abbDAO.doSave(abb);
-							}
-							String messageok = "Prodotto inserito con successo!";
-							request.setAttribute("messageok", messageok);
-							RequestDispatcher dispatcher = request.getRequestDispatcher(response.encodeURL("/servletgestioneadmin"));
-							dispatcher.forward(request, response);
+							request.setAttribute("message", "Capienza non disponibile");
+							String url="/servletgestioneadmin";
+							RequestDispatcher view=super.getServletContext().getRequestDispatcher(response.encodeURL(url));
+							view.forward(request, response);
+							
+							//ho visto
+							
 						}//fine else prodotto non e presente nel database
 						
 					} catch (SQLException e) {
@@ -226,7 +269,7 @@ public class ServletFormProdNuovoAdmin extends HttpServlet {
 				url= response.encodeURL(url);
 				response.sendRedirect(url);
 				return;
-			}
+				}
 		}
 	}
 
